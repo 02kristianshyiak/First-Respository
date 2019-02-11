5b4c9d1e23f4bca3dc61160b57c636bbc929f992
Kristian Shyiak <02kristian.shyiak@gulllakecs.org>
2019-02-11 13:00:44 -0500

    Add the Functions & Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -2,6 +2,7 @@
 # Kristian Shyiak
 # 2.5.19
 
+'''
 name = input("What is your name: ")
 
 def greeting():
@@ -9,3 +10,17 @@ def greeting():
     print("Nice to meet you")
 
 greeting()
+'''
+
+# 4.13.4: Functions and Variables
+# Kristian Shyiak
+# 2.11.19
+
+x = 406
+
+def print_something():
+    x = 3
+    print('\n', x)
+
+print('\n', x)
+print_something()
